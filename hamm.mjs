6151cc4806630426cb4f1f5aca8d27ffc357a20d
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2022-03-17 20:31:38 +0000

    Build command WIP

--- hamm.mjs
+++ hamm.mjs
@@ -5,6 +5,7 @@ import { hideBin } from 'yargs/helpers';
 import { cmdCreate } from './commands/create.mjs';
 import * as url from 'url';
 import * as path from 'path';
+import { cmdBuild } from './commands/build.mjs';
 
 const scriptPath = path.dirname(url.fileURLToPath(import.meta.url));
 
@@ -12,7 +13,17 @@ yargs(hideBin(process.argv))
   .command(
     'create [folder]',
     'Create new Home Assistant configuration project',
-    y => y.positional('folder', { describe: '' }).demandOption('folder'),
+    y => y.positional('folder', { describe: 'path to target folder (will be created)' }).demandOption('folder'),
     argv => cmdCreate(argv.folder, scriptPath))
+
+  .command(
+    'build [environment]',
+    'Build configuration for the specific environment',
+    y => y
+      .positional('environment', { describe: 'Target environment ("dev" or "prod")' })
+      .choices('environment', [ 'dev', 'prod' ])
+      .demandOption('environment'),
+    argv => cmdBuild(argv.environment))
+
   .demandCommand()
   .parse();
