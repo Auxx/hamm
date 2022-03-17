5d3e20242c4a011206ba844cbd1bca597202d080
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2022-03-17 17:30:23 +0000

    Create command WIP

---
+++ commands/create.mjs
@@ -0,0 +1,3 @@
+export async function cmdCreate(folder, scriptPath) {
+  console.log('cmdCreate', folder, scriptPath);
+}
--- hamm.mjs
+++ hamm.mjs
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { cmdCreate } from './commands/create.mjs';
+import * as url from 'url';
+import * as path from 'path';
+
+const scriptPath = path.dirname(url.fileURLToPath(import.meta.url));
+
+yargs(hideBin(process.argv))
+  .command(
+    'create [folder]',
+    'Create new Home Assistant configuration project',
+    y => y.positional('folder', { describe: '' }).demandOption('folder'),
+    argv => cmdCreate(argv.folder, scriptPath))
+  .demandCommand()
+  .parse();
