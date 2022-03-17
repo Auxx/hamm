d20598bef8a49673d82c416df0b47aa0ad785e2b
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2022-03-17 20:13:01 +0000

    Added check if git is installed

--- commands/create.mjs
+++ commands/create.mjs
@@ -1,15 +1,25 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
+import 'zx/globals';
 
 export async function cmdCreate(folder, scriptPath) {
+  console.log('\nChecking if git is installed...');
+
+  try {
+    await quiet($`git --version`);
+  } catch (e) {
+    console.log('Git not found! Please install git to continue https://git-scm.com/\n');
+    return;
+  }
+
   const templateSource = path.join(scriptPath, 'template');
 
-  console.log(`\nCreating new project in ${ folder } using template from ${ templateSource }...`);
+  console.log(`\nCreating new project in \x1b[36m${ folder }\x1b[0m using template from \x1b[36m${ templateSource }\x1b[0m...`);
 
   await fs.mkdir(folder, { recursive: true });
   await fs.cp(templateSource, folder, { recursive: true });
 
-  console.log('DONE!\n');
+  console.log('\nDONE!\n');
 
   console.log('You can now build the project in development mode:');
   console.log('    \x1b[37m$ npx hamm build dev\x1b[0m\n');
@@ -23,4 +33,5 @@ export async function cmdCreate(folder, scriptPath) {
   console.log('        -v /etc/localtime:/etc/localtime:ro \\');
   console.log('        ghcr.io/home-assistant/home-assistant:stable\x1b[0m\n');
   console.log('Don\'t forget to replace "Europe/London" with your local time zone!\n');
+
 }
