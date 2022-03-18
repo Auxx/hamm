d40deb196b957c002ac7743c3f69ab063e484d31
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2022-03-18 12:59:43 +0000

    Watch command stub

---
+++ commands/watch.mjs
@@ -0,0 +1,3 @@
+export async function cmdWatch(env) {
+  console.log('WATCH', env);
+}
--- hamm.mjs
+++ hamm.mjs
@@ -8,13 +8,14 @@ import { cmdCreate } from './commands/create.mjs';
 import { cmdBuild } from './commands/build.mjs';
 import { cmdClean } from './commands/clean.mjs';
 import { cmdDebug } from './commands/debug.mjs';
+import { cmdWatch } from './commands/watch.mjs';
 
 const scriptPath = path.dirname(url.fileURLToPath(import.meta.url));
 
 yargs(hideBin(process.argv))
   .command(
     'create [folder]',
-    'Create new Home Assistant configuration project',
+    'Create new Home Assistant configuration project.',
     y => y.positional('folder', { describe: 'path to target folder (will be created)' }).demandOption('folder'),
     argv => cmdCreate(argv.folder, scriptPath))
 
@@ -37,11 +38,20 @@ yargs(hideBin(process.argv))
 
   .command(
     'debug [environment]',
-    'Prints out current project settings and stuff',
+    'Prints out current project settings and stuff.',
     y => y
       .positional('environment', { describe: 'Target environment ("dev" or "prod", not specified for all)' })
       .choices('environment', [ 'dev', 'prod' ]),
     argv => cmdDebug(argv.environment))
+
+  .command(
+    'watch [environment]',
+    'Watches file changes and re-builds the project automatically.',
+    y => y
+      .positional('environment', { describe: 'Target environment ("dev" or "prod")' })
+      .choices('environment', [ 'dev', 'prod' ])
+      .demandOption('environment'),
+    argv => cmdWatch(argv.environment))
 
   .demandCommand()
   .parse();
